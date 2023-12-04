c8635601129b92801f4f509ba707f830ef9e58db
Carlos Muñoz <munozrengifocarlos@gmail.com>
2023-12-03 23:48:13 -0500

    feat: Initial audio player functions

---
+++ src/libs/audio-player.ts
@@ -0,0 +1,51 @@
+import { createAudioPlayer, AudioPlayerStatus, createAudioResource, type VoiceConnection, type AudioPlayer, type AudioResource } from "@discordjs/voice"
+import ytdl from "ytdl-core"
+
+export let audioPlayer: AudioPlayer | undefined
+export let audioResource: AudioResource | undefined
+export let volume: number = 0.2
+
+export function initAudioPlayer (connection: VoiceConnection): void {
+  audioPlayer = createAudioPlayer()
+  connection.subscribe(audioPlayer)
+
+  audioPlayer.on("error", (err) => {
+    console.error(`[AUDIO_PLAYER_ERROR]: ${err.stack}`)
+  })
+
+  audioPlayer.on(AudioPlayerStatus.Playing, (oldState, newState) => {
+    console.log("[AUDIO_PLAYER] Playing a song")
+  })
+
+  audioPlayer.on(AudioPlayerStatus.Idle, (oldState, newState) => {
+    if (oldState.status !== AudioPlayerStatus.Idle) {
+      audioResource = undefined
+    }
+    console.log("[AUDIO_PLAYER] Now is idle")
+  })
+}
+
+export function initAudioResource (URL: string): AudioResource {
+  const stream = ytdl(URL, { filter: "audioonly" })
+  audioResource = createAudioResource(stream, { inlineVolume: true })
+  audioResource.volume?.setVolume(volume)
+  return audioResource
+}
+
+export function setAudioPlayerVolume (value: number): boolean {
+  const isValidVolume = value >= 0 && value <= 100
+
+  if (isValidVolume) {
+    const parseValue = value / 100
+    audioResource?.volume?.setVolume(parseValue)
+    volume = parseValue
+  }
+
+  return isValidVolume
+}
+
+export function destroyAudioPlayer (): void {
+  if (typeof audioPlayer === "undefined") return
+  audioPlayer.stop()
+  audioPlayer = undefined
+}
